046fa05d2e085f8ea80858563e9ed31fc58fed29
Alperen Adalar <a.adalar@ecolarium.io>
2025-10-25 22:18:07 +0300

    Remove TestModel class and its DbSet from AppDbContext

--- ailab-super-app/Data/AppDbContext.cs
+++ ailab-super-app/Data/AppDbContext.cs
@@ -30,7 +30,6 @@ namespace ailab_super_app.Data
         public DbSet<AuditLog> AuditLogs { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
         public DbSet<RfidReader> RfidReaders { get; set; }
-        public DbSet<TestModel> TestModels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder b)
         {
--- ailab-super-app/Models/TestModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ailab_super_app.Models;
-
-public class TestModel
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
