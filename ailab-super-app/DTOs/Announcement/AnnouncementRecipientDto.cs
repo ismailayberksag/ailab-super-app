288dbc119bc0e40e91f9d74f33a3afd4a61a6198
ismailayberksag <ayberkismail0@gmail.com>
2025-10-28 16:27:10 +0300

    fix(announcements)

--- ailab-super-app/DTOs/Announcement/AnnouncementRecipientDto.cs
+++ ailab-super-app/DTOs/Announcement/AnnouncementRecipientDto.cs
@@ -4,7 +4,7 @@ namespace ailab_super_app.DTOs.Announcement
 {
     public class AnnouncementRecipientDto
     {
-        public Guid Id UserId { get; set; }
+        public Guid UserId { get; set; }
         public string FullName { get; set; } = default!;
         public string Email { get; set; } = default!;
         public bool IsRead { get; set; }
