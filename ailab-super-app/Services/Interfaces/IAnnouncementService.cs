8ee79fac5f30bed7701689d37af8e16d4568323d
ismailayberksag <ayberkismail0@gmail.com>
2025-10-28 15:54:11 +0300

    feat(announcements): add ınterface for announcement service

--- ailab-super-app/Services/Interfaces/IAnnouncementService.cs
+++ ailab-super-app/Services/Interfaces/IAnnouncementService.cs
@@ -1,6 +1,15 @@
-﻿namespace ailab_super_app.Services.Interfaces
+﻿using System;
+using System.Threading.Tasks;
+using ailab_super_app.DTOs.Announcement;
+using ailab_super_app.Helpers;
+
+namespace ailab_super_app.Services.Interfaces
 {
     public interface IAnnouncementService
     {
+        Task<Guid> CreateAsync(Guid actorUserId, CreateAnnouncementDto dto);
+        Task MarkAsReadAsync(Guid announcementId, Guid userId);
+        Task<PagedResult<AnnouncementListDto>> GetMyAnnouncementsAsync(Guid userId, PaginationParams pagination, bool? isRead = null);
+        Task<AnnouncementDto> GetByIdAsync(Guid id, Guid requesterId);
     }
 }
